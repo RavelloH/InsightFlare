8f4eb09634d1c1746d6738e596180860e99bbe8e
RavelloH <ravellohh@gmail.com>
2026-03-03 12:46:56 +0800

    feat: ui 基础组件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "InsightFlare API",
